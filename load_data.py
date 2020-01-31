4783b04b365dd039decc96f97a58145b75903582
Seungki Kim <seungki.kim@vtouch.io>
2020-01-31 09:30:26 +0000

    Detect face with MTCNN

--- load_data.py
+++ load_data.py
@@ -5,6 +5,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 import cv2
 import json
+from facenet_pytorch import MTCNN, InceptionResnetV1
+from PIL import Image
+
+mtcnn = MTCNN()
 
 DIR = '/workspace/code/dfdc_train_part_0'
 
@@ -20,3 +24,16 @@ with open(meta_path) as f:
             ret, frame = capture_image.read()
             if ret is False:
                 break
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = Image.fromarray(frame)
+            face = mtcnn(frame)
+            if face is None:
+                continue
+
+            face = np.transpose(face, (1, 2, 0))
+            face = np.array(face)
+            face = (face + 1.0)/2.0
+
+            plt.clf()
+            plt.imshow(face)
+            plt.pause(.1)
