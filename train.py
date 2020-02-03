59c2203194568cdbdab9dbd9d27963648031b21d
Seungki Kim <seungki.kim@vtouch.io>
2020-02-03 08:47:33 +0000

    Load pakcages

---
+++ train.py
@@ -0,0 +1,12 @@
+import os, sys, random
+import numpy as np
+import pandas as pd
+import cv2
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from tqdm.notebook import tqdm
+
+import matplotlib.pyplot as plt
