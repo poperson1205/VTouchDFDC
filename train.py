dd0f382410cb3de889eadf9f32ed48bc952d88f3
Seungki Kim <seungki.kim@vtouch.io>
2020-02-04 02:11:50 +0000

    Read metadata.csv for the face dataset

--- train.py
+++ train.py
@@ -11,5 +11,13 @@ from tqdm.notebook import tqdm
 
 import matplotlib.pyplot as plt
 
+crops_dir = '../deep-faces/faces_155'
+
 gpu = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(gpu)
+
+metadata_df = pd.read_csv('../deep-faces/metadata.csv')
+metadata_df.head()
+
+print('# REAL: {0}'.format(len(metadata_df[metadata_df.label == 'REAL'])))
+print('# FAKE: {0}'.format(len(metadata_df[metadata_df.label == 'FAKE'])))
+print('TOTAL: {0}'.format(len(metadata_df)))
