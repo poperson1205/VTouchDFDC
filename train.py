9cea9f3a3a2750aab53d773da5bba599ccaa7aca
Seungki Kim <seungki.kim@vtouch.io>
2020-02-04 07:48:48 +0000

    Train network

--- train.py
+++ train.py
@@ -80,7 +80,52 @@ def create_data_loaders(crops_dir, metadata_df, image_size, batch_size, num_work
 
 train_loader, val_loader = create_data_loaders(crops_dir, metadata_df, image_size, batch_size, num_workers=0)
 
-image_tensor, class_index = next(iter(train_loader))
-plt.imshow(image_tensor[0].permute(1, 2, 0))
-plt.pause(1)
-print(class_index[0])
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.avg_pool = nn.AvgPool2d(53)
+        self.fc = nn.Linear(16, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.avg_pool(self.pool(F.relu(self.conv2(x))))
+        x = x.view(-1, 16)
+        x = F.relu(self.fc(x))
+        return x
+
+net = Net()
+
+
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):
+    
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 1421 == 1420:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+torch.save(net.state_dict(), 'binary_classifier.pth')
