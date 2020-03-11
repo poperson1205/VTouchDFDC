173baf076f983141e3594f856fa7cb085ea47211
Seungki Kim <seungki.kim@vtouch.io>
2020-03-11 10:21:29 +0000

    Train siamese net

--- siamese.py
+++ siamese.py
@@ -3,31 +3,20 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from efficientnet_pytorch import EfficientNet
 
 class Siamese(nn.Module):
 
     def __init__(self):
         super(Siamese, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(1, 64, 10),  # 64@96*96
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  # 64@48*48
-            nn.Conv2d(64, 128, 7),
-            nn.ReLU(),    # 128@42*42
-            nn.MaxPool2d(2),   # 128@21*21
-            nn.Conv2d(128, 128, 4),
-            nn.ReLU(), # 128@18*18
-            nn.MaxPool2d(2), # 128@9*9
-            nn.Conv2d(128, 256, 4),
-            nn.ReLU(),   # 256@6*6
-        )
-        self.liner = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
-        self.out = nn.Linear(4096, 1)
+        self.efficient_net = EfficientNet.from_pretrained('efficientnet-b0', num_classes=1)
+        self.linear = nn.Sequential(nn.AdaptiveAvgPool2d(1), nn.Sigmoid())
+        self.out = nn.Linear(1280, 1)
 
     def forward_one(self, x):
-        x = self.conv(x)
+        x = self.efficient_net.extract_features(x)
+        x = self.linear(x)
         x = x.view(x.size()[0], -1)
-        x = self.liner(x)
         return x
 
     def forward(self, x1, x2):
--- train_siamese.py
+++ train_siamese.py
@@ -12,24 +12,27 @@ from torchvision.transforms import Normalize
 from tqdm import tqdm
 
 import matplotlib.pyplot as plt
-from efficientnet_pytorch import EfficientNet
+from siamese import Siamese
 
 from tqdm import tqdm
 
 ROOT_DIR = '/media/vtouchinc02/database/RawData/deepfake-32frame/'
+CSV_DIR = '/media/vtouchinc02/database/RawData/deepfake-32frame-csv/'
 
 # Read dataframe
 list_metadata_df = []
-list_folder_index = range(0, 50, 5)
+list_metadata_real_df = []
+list_folder_index = range(2, 48)
 for i in list_folder_index:
-    folder_name = 'dfdc_train_part_%d' % i
-    list_metadata_df.append(pd.read_csv('metadata_%d.csv' % i))
-metadata_df = pd.concat(list_metadata_df)
+    list_metadata_df.append(pd.read_csv(os.path.join(CSV_DIR, 'metadata_%d.csv' % i)))
+    list_metadata_real_df.append(pd.read_csv(os.path.join(CSV_DIR, 'metadata_real_%d.csv' % i)))
+metadata_df = pd.concat(list_metadata_df).sample(frac=0.05).reset_index(drop=True)
+metadata_real_df = pd.concat(list_metadata_real_df).sample(frac=0.3).reset_index(drop=True)
 
 gpu = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 image_size = 224
-batch_size = 64
+batch_size = 32
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 normalize_transform = Normalize(mean,std)
@@ -47,40 +50,52 @@ from torch.utils.data import Dataset
 
 class VideoDataset(Dataset):
     
-    def __init__(self, root_dir, df, image_size=224):
+    def __init__(self, root_dir, df_fake, df_real, image_size=224):
         self.root_dir = root_dir
         self.image_size = image_size
-        self.df = df
-        
-        num_fake_imgs = len(df)
+        self.df_fake = df_fake
+        self.df_real = df_real
+        self.df_real_shuffled = df_real.sample(frac=1).reset_index(drop=True)
+
+        num_fake_imgs = len(self.df_fake)
+        num_real_imgs = len(self.df_real)
         print('# fake images: %d' % num_fake_imgs)
+        print('# real images: %d' % num_real_imgs)
 
     def __getitem__(self, index):
-        is_fake = True if index % 2 == 0 else False
-        class_index = 1 if is_fake else 0
-        row = self.df.iloc[int(index/2)]
-        file_name = row['image_name'] if is_fake else row['original']
-        image_tensor = load_image_as_tensor(os.path.join(self.root_dir, file_name), self.image_size)
-        return image_tensor, class_index
+        real_real_pair = True if index % 2 == 0 else False
+        class_index = 1 if real_real_pair else 0
+        if real_real_pair:
+            row1 = self.df_real.iloc[int(index/2)]
+            row2 = self.df_real_shuffled.iloc[int(index/2)]
+            name1 = row1['image_name']
+            name2 = row2['image_name']
+        else:
+            row = self.df_fake.iloc[int(index/2)]
+            name1 = row['image_name']
+            name2 = row['original']
+        image_tensor1 = load_image_as_tensor(os.path.join(self.root_dir, name1), self.image_size)
+        image_tensor2 = load_image_as_tensor(os.path.join(self.root_dir, name2), self.image_size)
+        return image_tensor1, image_tensor2, class_index
 
     def __len__(self):
-        return len(self.df) * 2
+        return min(len(self.df_fake), len(self.df_real)) * 2
 
 
 
 from torch.utils.data import DataLoader
 
-def create_data_loaders(root_dir, metadata_df, image_size, batch_size, num_workers):
-    dataset = VideoDataset(root_dir, metadata_df, image_size)
+def create_data_loaders(root_dir, metadata_df, metadata_real_df, image_size, batch_size, num_workers):
+    dataset = VideoDataset(root_dir, metadata_df, metadata_real_df, image_size)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
     
     return loader
 
-train_loader = create_data_loaders(ROOT_DIR, metadata_df, image_size, batch_size, num_workers=0)
+train_loader = create_data_loaders(ROOT_DIR, metadata_df, metadata_real_df, image_size, batch_size, num_workers=0)
 
 
 
-model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=1)
+model = Siamese()
 model.cuda()
 
 
@@ -90,19 +105,20 @@ import torch.optim as optim
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(3):
+for epoch in range(1):
     
     bce_loss = 0.0
     total_examples = 0
 
     for data in tqdm(train_loader):
         batch_size = data[0].shape[0]
-        x = data[0].to(gpu)
-        y_true = data[1].to(gpu).float()
+        x1 = data[0].to(gpu)
+        x2 = data[1].to(gpu)
+        y_true = data[2].to(gpu).float()
         
         optimizer.zero_grad()
 
-        y_pred = model(x)
+        y_pred = model(x1, x2)
         y_pred = y_pred.squeeze()
 
         loss = F.binary_cross_entropy_with_logits(y_pred, y_true)
@@ -121,4 +137,4 @@ for epoch in range(3):
 
 print('Finished Training')
 
-torch.save(model.state_dict(), 'binary_classifier.pth')
+torch.save(model.state_dict(), 'binary_classifier_siamese.pth')
